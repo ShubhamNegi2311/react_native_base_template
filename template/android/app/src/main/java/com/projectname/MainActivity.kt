458081cbafcabc7ea6c6c07413043c1c3b78197b
Shubham Negi <shubhamnegi@weexcel.in>
2024-11-21 17:32:00 +0530

    updated some changes for Project Name

--- template/android/app/src/main/java/com/projectname/MainActivity.kt
+++ template/android/app/src/main/java/com/projectname/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.rn_template
+package com.projectname
 
-import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -17,7 +16,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "rn_template"
+  override fun getMainComponentName(): String = "ProjectName"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- template/android/app/src/main/java/com/projectname/MainApplication.kt
+++ template/android/app/src/main/java/com/projectname/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.rn_template
+package com.projectname
 
 import android.app.Application
 import com.facebook.react.PackageList
