f95db3cd167b323507daa1296dd7ccb5e06e08df
Shubham Negi <shubhamnegi@weexcel.in>
2025-01-03 14:41:19 +0530

    post init script updated.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log("This is post init script");
+console.log(
+  "This is post init script",
+  "Congrats you have successfully created the project with this template."
+);
