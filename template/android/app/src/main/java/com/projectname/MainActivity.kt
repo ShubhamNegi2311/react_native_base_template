181ef9e336f5834283f923ac04af70663be49dfb
Shubham Negi <shubhamnegi@weexcel.in>
2025-02-19 15:56:26 +0530

    android fix added for missed imports.

--- template/android/app/src/main/java/com/projectname/MainActivity.kt
+++ template/android/app/src/main/java/com/projectname/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.projectname
 
+import android.os.Bundle
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
